997ffd45dc6a63b231079efe7d2af561cbc6f9e0
Junyi Xu <junyixu0@gmail.com>
2022-10-16 23:15:42 +0800

    init

---
+++ code/test1.c
@@ -0,0 +1,14 @@
+/*
+ * test1.c
+ * Copyright (C) 2021 junyi <junyixu0@gmail.com>
+ * 
+ * Distributed under terms of the MIT license.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int a;
+	return 0;
+}
